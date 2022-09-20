9ee1ae3cb10b8f4dfccdfbf9817bf8998fc85fd0
yovanproev <yovanproev@yahoo.com>
2022-09-20 08:57:44 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double aa = 5;
-        double bb = 10;
+        double[] leftVals = {100.0, 25.0, 225.0, 11.0};
+        double[] rightVals = {50.0, 92.0, 17.0, 3.0};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if (aa < bb) {
-            System.out.println("NOOOOO!");
-        }
+        if (args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double currentResult : results)
+                System.out.println(currentResult);
+        } else if(args.length == 1 && args[0].equals("interactive")) {
+            executeInteractively();
+        } else if(args.length == 3)
+            handleCommandLine(args);
         else
-        System.out.println("Hello world!");
+        System.out.println("Please provide operation code and 2 numeric values");
     }
-    static void execute() {
-        System.out.println("Blak");
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = leftVal / rightVal;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0;
+                break;
+        }
+        return result;
+    }
+
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers: ");
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         String[] parts = userInput.split(" ");
@@ -24,6 +65,28 @@ public class Main {
         double leftVal = valueFromWord(parts[1]);
         double rightVal = valueFromWord(parts[2]);
         double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
     }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for (int index = 0; index < numberWords.length; index++) {
+            if (word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
+    }
+
+    static char opCodeFromString(String operationName) {
+        return operationName.charAt(0);
+    }
+
+
 }
 
